eb59cb36749f1d69a94b63ea29890c394be7ca87
Davi Hoffmann Takahashi Albert <128089622+davitalbert1@users.noreply.github.com>
2026-05-05 20:02:43 -0300

    Melhorias
    Mudança de posição de teletransporte de fase e adição de texto de fase.

---
+++ Assets/Script/floatingAnimation.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class FloatingText : MonoBehaviour
+{
+    public float amplitude = 0.5f; // altura do movimento
+    public float frequency = 2f;   // velocidade da oscilação
+
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        float offsetY = Mathf.Sin(Time.time * frequency) * amplitude;
+        transform.position = startPos + new Vector3(0, offsetY, 0);
+    }
+}
