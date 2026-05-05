702bc781aea3a636926ebb290206a8ec9f569f2d
Davi Hoffmann Takahashi Albert <128089622+davitalbert1@users.noreply.github.com>
2026-05-04 21:43:02 -0300

    teletransporte
    adição de teletransporte entre fases

--- Assets/Script/MessageTrigger.cs
+++ Assets/Script/MessageTrigger.cs
@@ -1,14 +1,24 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MessageTrigger : MonoBehaviour
 {
+    [Header("Configurações da Mensagem")]
     public string mensagem = "O jogador entrou na área!";
 
+    [Header("Configurações de Teleporte")]
+    public Object cenaParaArrastar;
+
+    [Header("Visualização (Editor)")]
     public Color corDoGizmo = new Color(0, 1, 0, 0.3f);
 
     private void OnTriggerEnter(Collider other){
         if (other.CompareTag("Player")){
             Debug.Log("<color=cyan>[Trigger]</color> " + mensagem);
+
+            if (cenaParaArrastar != null){
+                SceneManager.LoadScene(cenaParaArrastar.name);
+            }
         }
     }
 
@@ -21,3 +31,4 @@ public class MessageTrigger : MonoBehaviour
         Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
     }
 }
+
